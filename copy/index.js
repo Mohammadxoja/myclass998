6bbb8e2683bfb1a941d2d4b72cc52d6c1482dfff
Mohammadxoja <75994491+Mohammadxoja@users.noreply.github.com>
2021-01-28 01:14:57 +0500

    Delete copy directory

--- copy/index.js
+++
@@ -1,8 +0,0 @@
-
-	const btn = document.querySelector('button');   
-    const img = document.querySelectorAll('img'); 
-	btn.addEventListener('click', () => {
-       for(let i = 0; i < img.length; i++) {
-       	 img[i].classList.toggle('block'); 
-       } 
-	})
